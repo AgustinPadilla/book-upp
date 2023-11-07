bbecb2aa7185d55a84a48cbaff6900759f5caa5c
Agustin Padilla <agustin0padilla2001@gmail.com>
2023-11-07 13:07:28 -0300

    fix http?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useBooks } from './hooks/useBooks'
 import { ReadList } from './components/ReadList'
 import { BooksGrid } from './components/BooksGrid'
@@ -10,7 +9,7 @@ function App () {
   return (
     <main className='w-full min-h-screen flex justify-around text-amber-950 relative'>
       <div className='flex flex-col items-center flex-grow'>
-        <div className='max-w-6xl bg-lightbeige h-full mx-3 md:mx-0 md:w-10/12 shadow-2xl flex flex-col items-center my-10 rounded-3xl'>
+        <div className='max-w-6xl bg-lightbeige h-full mx-3 md:mx-0 sm:w-10/12 shadow-2xl flex flex-col items-center my-10 rounded-3xl'>
           <h1 className='text-5xl font-bold  m-10 drop-shadow-md'>Book Upp!</h1>
           <Search />
           {books.length < 1
--- src/components/ReadList.jsx
+++ src/components/ReadList.jsx
@@ -13,10 +13,10 @@ export const ReadList = () => {
 
   return (
     <div className='fixed right-0'>
-      <button onClick={toggleSideBar} className='absolute z-10 top-0 right-4 md:-left-12 mt-5 text-2xl bg-lightbeige p-2 rounded-full shadow-2xl md:opacity-90 hover:opacity-100'>
+      <button onClick={toggleSideBar} className='absolute top-0 z-10 right-2 md:right-auto md:-left-12 mt-5 text-2xl bg-lightbeige p-2 rounded-full md:shadow-2xl md:opacity-90 hover:opacity-100'>
         {hidden ? <ReadListIcon /> : <CloseIcon />}
       </button>
-      <div className={`h-screen md:w-80 relative shadow-xl gap-3 p-2 bg-lightbeige md:bg-opacity-90 text-md overflow-auto font-semibold ${hidden && 'hidden'}`}>
+      <div className={`h-screen w-screen md:w-80 relative shadow-xl gap-3 p-2 bg-lightbeige md:bg-opacity-90 text-md overflow-auto font-semibold ${hidden && 'hidden'}`}>
         <h2 className='text-2xl font-bold drop-shadow-2xl my-4 w-full text-center'>Read List</h2>
         <div className='flex flex-col-reverse gap-2 items-center'>
           {readList.map(book => {
